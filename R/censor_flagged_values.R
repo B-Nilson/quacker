ce960587d4f4c98e4983fcd3ce80ef34fe921bfe
B-Nilson <nilson.brayden@gmail.com>
2025-12-10 20:19:57 -0800

    add function to censor values flagged by qaqc functions

---
+++ R/censor_flagged_values.R
@@ -0,0 +1,19 @@
+censor_flagged_values <- function(
+  qaqced_data,
+  value_cols,
+  censored_to = "{.col}_censored",
+  flag_prefix = ".flag_"
+) {
+  value_cols <- value_cols |>
+    guess_value_cols(data = qaqced_data) |>
+    handle_tidyselect(data = qaqced_data, .expect_some = TRUE)
+
+  qaqced_data |>
+    dplyr::mutate(
+      dplyr::across(
+        value_cols,
+        \(x) ifelse(get(paste0(flag_prefix, dplyr::cur_column())) == 0, x, NA),
+        .names = censored_to
+      )
+    )
+}
